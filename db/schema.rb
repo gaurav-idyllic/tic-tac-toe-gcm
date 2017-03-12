0a686a77379c7424a15b95e580f56fb391b340cc
Gaurav Mamidwar <gaurav.mamidwar@idyllic-software.com>
2017-03-12 15:17:16 +0530

    db pg added

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170225191913) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cells", force: :cascade do |t|
     t.integer  "value"
     t.integer  "game_id"
@@ -30,11 +33,6 @@ ActiveRecord::Schema.define(version: 20170225191913) do
     t.datetime "updated_at",             null: false
   end
 
-  create_table "moves", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -50,7 +48,7 @@ ActiveRecord::Schema.define(version: 20170225191913) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
